e5803b6a5841850396fe5363a0cc66a8afb4ad88
Romain ROCH <romain.roch2020@protonmail.com>
2022-05-19 16:30:16 +0200

    Commit Init

---
+++ src/main/java/fr/rroch/chifumi/ChifumiApplication.java
@@ -0,0 +1,13 @@
+package fr.rroch.chifumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChifumiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChifumiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/rroch/chifumi/ChifumiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rroch.chifumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChifumiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
